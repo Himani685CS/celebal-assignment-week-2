782b22fd3f1dedfdac93aea17d6a0060d7328826
HIMANI SUJANE <121811029+Himani685CS@users.noreply.github.com>
2024-06-11 19:35:42 +0530

    Create Create a view vwCustomerOrders which returns CompanyName.OrderlD.OrderDate. ProductID.ProductName.Quantity.UnitPrice.Quantity * od.UnitPrice.sql

---
+++ Create a view vwCustomerOrders which returns CompanyName.OrderlD.OrderDate. ProductID.ProductName.Quantity.UnitPrice.Quantity * od.UnitPrice.sql
@@ -0,0 +1,15 @@
+CREATE VIEW vwCustomerOrders AS
+SELECT 
+    c.CompanyName,
+    o.OrderID,
+    o.OrderDate,
+    od.ProductID,
+    p.Name AS ProductName,
+    od.OrderQty AS Quantity,
+    od.UnitPrice,
+    (od.OrderQty * od.UnitPrice) AS TotalPrice
+FROM 
+    Sales.Customer c
+    JOIN Sales.SalesOrderHeader o ON c.CustomerID = o.CustomerID
+    JOIN Sales.SalesOrderDetail od ON o.SalesOrderID = od.SalesOrderID
+    JOIN Production.Product p ON od.ProductID = p.ProductID;
